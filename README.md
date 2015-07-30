1415ec79fb832f6221d83d373b0d6c4a635c23cb
wang-q <wang-q@outlook.com>
2015-07-31 03:01:31 +0800

    Misc changes

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ App::Fasops - operating blocked fasta files
 
 # SYNOPSIS
 
+      commands: list the application's commands
+          help: display a command's help screen
+    
+       axt2fas: convert axt to blocked fasta
+       maf2fas: convert maf to blocked fasta
+         names: scan a blocked fasta file and output all names
+         split: split a blocked fasta file to seperate per-alignment files
+        subset: extract a subset of names from a blocked fasta
+
 See `fasops commands` for usage information.
--- lib/App/Fasops.pm
+++ lib/App/Fasops.pm
@@ -180,6 +180,15 @@ App::Fasops - operating blocked fasta files
 
 =head1 SYNOPSIS
 
+    commands: list the application's commands
+        help: display a command's help screen
+  
+     axt2fas: convert axt to blocked fasta
+     maf2fas: convert maf to blocked fasta
+       names: scan a blocked fasta file and output all names
+       split: split a blocked fasta file to seperate per-alignment files
+      subset: extract a subset of names from a blocked fasta
+
 See C<fasops commands> for usage information.
 
 =cut
