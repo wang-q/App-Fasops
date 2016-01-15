be704012282bd04edb1978d099b48f8c8d963179
wang-q <wang-q@outlook.com>
2016-01-15 17:18:22 +0800

    Misc changes

--- README.md
+++ README.md
@@ -4,25 +4,33 @@ App::Fasops - operating blocked fasta files
 
 # SYNOPSIS
 
+    fasops <command> [-?h] [long options...]
+        -? -h --help    show help
+
+    Available commands:
+
       commands: list the application's commands
           help: display a command's help screen
-    
+
        axt2fas: convert axt to blocked fasta
         covers: scan blocked fasta files and output covers on chromosomes
+         links: scan blocked fasta files and output links between pieces
        maf2fas: convert maf to blocked fasta
          names: scan blocked fasta files and output all species names
+       replace: replace headers from a blocked fasta
       separate: separate blocked fasta files by species
          split: split blocked fasta files to separate per-alignment files
-        subset: extract a subset of names from a blocked fasta
+        subset: extract a subset of species from a blocked fasta
 
 See `fasops commands` for usage information.
 
-# LICENSE
-
-Copyright 2014- Qiang Wang
-
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-
 # AUTHOR
 
-Qiang Wang
+Qiang Wang &lt;wang-q@outlook.com>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Qiang Wang.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/App/Fasops.pm
+++ lib/App/Fasops.pm
@@ -1,6 +1,6 @@
 package App::Fasops;
 
-our $VERSION = '0.3.1';
+our $VERSION = '0.3.2';
 
 use App::Cmd::Setup -app;
 
@@ -14,29 +14,35 @@ App::Fasops - operating blocked fasta files
 
 =head1 SYNOPSIS
 
-    commands: list the application's commands
-        help: display a command's help screen
-  
-     axt2fas: convert axt to blocked fasta
-      covers: scan blocked fasta files and output covers on chromosomes
-     maf2fas: convert maf to blocked fasta
-       names: scan blocked fasta files and output all species names
-    separate: separate blocked fasta files by species
-       split: split blocked fasta files to separate per-alignment files
-      subset: extract a subset of names from a blocked fasta
+    fasops <command> [-?h] [long options...]
+    	-? -h --help    show help
+
+    Available commands:
+
+      commands: list the application's commands
+          help: display a command's help screen
+
+       axt2fas: convert axt to blocked fasta
+        covers: scan blocked fasta files and output covers on chromosomes
+         links: scan blocked fasta files and output links between pieces
+       maf2fas: convert maf to blocked fasta
+         names: scan blocked fasta files and output all species names
+       replace: replace headers from a blocked fasta
+      separate: separate blocked fasta files by species
+         split: split blocked fasta files to separate per-alignment files
+        subset: extract a subset of species from a blocked fasta
 
 See C<fasops commands> for usage information.
 
-=cut
-
-=head1 LICENSE
-
-Copyright 2014- Qiang Wang
-
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-
 =head1 AUTHOR
 
-Qiang Wang
+Qiang Wang <wang-q@outlook.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Qiang Wang.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
